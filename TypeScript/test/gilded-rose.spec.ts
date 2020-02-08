17f8bcf36466cc54456dca3a12388b8cb6f2fe5d
Juan Sánchez <jsmontesinos@gmail.com>
2020-02-08 19:41:09 +0100

    refactoring - Add test for each requirement #refactoring

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,15 +1,83 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
+const AGED_BRIE = 'Aged Brie';
+const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+const FOO = 'foo';
+const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+
 describe('Gilded Rose', function () {
 
-    it('should lower selling and quality values of products', function() {
-        const gildedRose = new GildedRose([ new Item('foo1', 4, 4), new Item('foo2', 5, 6) ]);
+    it('should decrease sellin and quality values of products', function() {
+        const gildedRose = new GildedRose([ new Item('foo1', 4, 4) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].sellIn).to.equal(3);
-        expect(items[1].sellIn).to.equal(4);
         expect(items[0].quality).to.equal(3);
-        expect(items[1].quality).to.equal(5);
     });
 
+    it('should decrease quality twice as fast if sell by date has passed', function() {
+        const gildedRose = new GildedRose([ new Item(FOO, 1, 4), new Item('foo1', 0, 4) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(0);
+        expect(items[1].sellIn).to.equal(-1);
+        expect(items[0].quality).to.equal(3);
+        expect(items[1].quality).to.equal(2);
+    });
+
+    it('should keep quality over 0', function() {
+        const gildedRose = new GildedRose([ new Item(FOO, 1, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(0);
+        expect(items[0].quality).to.gte(0);
+    });
+
+    it('should increase Aged Brie quality the older it gets', () => {
+        const gildedRose = new GildedRose([ new Item(AGED_BRIE, 1, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(0);
+        expect(items[0].quality).to.equal(1);
+    });
+
+    it('should keep quality under 50', () => {
+        const gildedRose = new GildedRose([ new Item(FOO, 2, 50) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(1);
+        expect(items[0].quality).to.lte(50);
+    });
+
+    it('should keep Sulfuras quality and never sold', () => {
+        const gildedRose = new GildedRose([ new Item(SULFURAS, 0, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.gte(0);
+        expect(items[0].quality).to.equal(40);
+    });
+
+    it('should increase quality of backstage passes by 3 when sellin is 5 days or less', () => {
+        const gildedRose = new GildedRose([ new Item(BACKSTAGE_PASSES, 4, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(3);
+        expect(items[0].quality).to.equal(43);
+    });
+
+
+    it('should increase quality of backstage passes by 2 when sellin is 10 days or less', () => {
+        const gildedRose = new GildedRose([ new Item(BACKSTAGE_PASSES, 7, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(6);
+        expect(items[0].quality).to.equal(42);
+    });
+
+    it('should increase quality of backstage passes by 3 when sellin is 5 days or less', () => {
+        const gildedRose = new GildedRose([ new Item(BACKSTAGE_PASSES, 4, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(3);
+        expect(items[0].quality).to.equal(43);
+    });
+
+    it('should drop quality to 0 of backstage passes when sellin date has passed', () => {
+        const gildedRose = new GildedRose([ new Item(BACKSTAGE_PASSES, 0, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(-1);
+        expect(items[0].quality).to.equal(0);
+    });
 });
