1f467f3d5eb39e85919ea34ca879345c7b5f8ec5
Juan Sánchez <jsmontesinos@gmail.com>
2020-02-08 14:47:56 +0100

    refactoring - Add a test, sellin and quality should be decreased each day #refactoring

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -3,10 +3,13 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('should lower selling and quality values of products', function() {
+        const gildedRose = new GildedRose([ new Item('foo1', 4, 4), new Item('foo2', 5, 6) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].sellIn).to.equal(3);
+        expect(items[1].sellIn).to.equal(4);
+        expect(items[0].quality).to.equal(3);
+        expect(items[1].quality).to.equal(5);
     });
 
 });
